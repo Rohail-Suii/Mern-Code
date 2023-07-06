3d1390fca10c3cf2becf39b8987a5b71102007a0
Rohail-Suii <125988921+Rohail-Suii@users.noreply.github.com>
2023-07-06 16:30:43 +0500

    hamburger menu ready

--- React/my-app/src/shared/components/Navigation/MainNavigation.js
+++ React/my-app/src/shared/components/Navigation/MainNavigation.js
@@ -5,17 +5,30 @@ import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import "./MainNavigation.css";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = React.useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />
+      {drawerIsOpen ? (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      ) : null}
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           {/* 3 span are hamburger menu icon */}
           <span />
           <span />
