a7e901fdb8ebb50d16f4335a2aff2e6ee40b4259
Rohail-Suii <125988921+Rohail-Suii@users.noreply.github.com>
2023-07-06 15:56:44 +0500

    hamburger menu getting ready

--- React/my-app/src/shared/components/Navigation/MainNavigation.js
+++ React/my-app/src/shared/components/Navigation/MainNavigation.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import MainHeader from './MainHeader';
-import NavLinks from './NavLinks';
-import './MainNavigation.css';
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import "./MainNavigation.css";
+import SideDrawer from "./SideDrawer";
 
-const MainNavigation = props => {
+const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        {/* 3 span are hamburger menu icon */}
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      {/* nav links later */}
-      <nav>
-        <NavLinks/>
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          {/* 3 span are hamburger menu icon */}
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        {/* nav links later */}
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
