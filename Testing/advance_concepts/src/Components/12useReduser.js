7dcdf4cad5f996997bfe35d91d230f231a823d4b
Rohail-Suii <125988921+Rohail-Suii@users.noreply.github.com>
2023-07-15 09:54:20 +0500

    useReducer hook

--- Testing/advance_concepts/src/Components/12useReduser.js
+++ Testing/advance_concepts/src/Components/12useReduser.js
@@ -1,27 +1,26 @@
 import React, { useReducer } from "react";
 
 const reducer = (state, action) => {
-    switch(action.type){
-        case "increment":
-            return {money: state.money + 10}
-        case "decrement":
-            return {money: state.money - 10}
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case "increment":
+      return { money: state.money + 10 };
+    case "decrement":
+      return { money: state.money - 10 };
+    default:
+      return state;
+  }
+};
 
 export default function UseReducer1() {
-    const [state,dispatch] = useReducer(reducer, {money: 100})
+  const [state, dispatch] = useReducer(reducer, { money: 100 });
 
-    return(
-        <div>
-            <h1>wallet : {state.money}</h1>
-            <div>
-                <button onClick={() => dispatch({type: "increment"})}>Add</button>
-                <button onClick={() => dispatch({type: "decrement"})}>Remove</button>
-
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>wallet : {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({ type: "increment" })}>Add</button>
+        <button onClick={() => dispatch({ type: "decrement" })}>Remove</button>
+      </div>
+    </div>
+  );
 }
